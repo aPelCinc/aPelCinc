d0c4ce1c0dddb1ae9f9554d9ffa4b8b2cd5571bb
Àlex Aguilera Ullés <aaguilera@iconsl.com>
2022-04-25 18:18:35 +0200

    socket create room

--- routes/sockets.js
+++ routes/sockets.js
@@ -2,7 +2,16 @@ let params = "hostname=aa&lobbyname=aaa"
 
 function controller(io) {
     io.on('connection', (socket) => {
+
+      io.of("/").adapter.on("create-room", (room) => {
+        console.log(`room ${room} was created`);
+      });
       
+      io.of("/").adapter.on("join-room", (room, id) => {
+        console.log(`socket ${id} has joined room ${room}`);
+      });
+        // let lobby = JSON.parse('{"' + decodeURI(params).replace(/&/g, '","').replace(/\+/g, '" "').replace(/=/g, '":"') + '"}')
+        // // console.log(lobby);
         // socket.emit('join', lobby, function(err) {
         //   if(err){
         //     alert(err);
@@ -13,10 +22,10 @@ function controller(io) {
         // })
 
 
-        console.log('a user connected');
-        socket.on('disconnect', () => {
-          console.log('user disconnected');
-        });
+        // console.log('a user connected');
+        // socket.on('disconnect', () => {
+        //   console.log('user disconnected');
+        // });
       });
 
       return io;
